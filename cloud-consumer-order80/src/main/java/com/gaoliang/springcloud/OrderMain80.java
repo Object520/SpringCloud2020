c0cedfbbd8fb601b72536e7481d047a9c62946fc
admin <gaoliangyou502@163.com>
2020-10-25 15:42:10 +0800

    将支付和订单服务注册到eureka server

--- cloud-consumer-order80/src/main/java/com/gaoliang/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/gaoliang/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.gaoliang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /*
  * Description: 支付模块启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: 2020/10/25 12:22
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
