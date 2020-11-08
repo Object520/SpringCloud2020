cf219aeae7084e9616450205fd21f62533e140b2
admin <gaoliangyou502@163.com>
2020-11-08 18:01:03 +0800

    降级方法与业务逻辑耦合-feignclient fallback

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/gaoliang/service/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.gaoliang.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+    @Override
+    public String paymentInfoOK(Integer id) {
+        return "------PaymentFallbackService fall back-paymentInfoOK";
+    }
+
+    @Override
+    public String paymentInfoTimeout(Integer id) {
+        return "------PaymentFallbackService fall back-paymentInfoTimeout";
+    }
+}
