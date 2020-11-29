2dfc29ec339bbd85c6369a92f4b90355fe6ca8dd
admin <gaoliangyou502@163.com>
2020-11-29 23:36:00 +0800

    消息总线-Spring Cloud Bus动态刷新全局广播配置

--- cloud-config-center-3344/src/main/java/com/gaoliang/springcloud/configserver/ConfigServerMain3344.java
+++ cloud-config-center-3344/src/main/java/com/gaoliang/springcloud/configserver/ConfigServerMain3344.java
@@ -2,11 +2,12 @@ package com.gaoliang.springcloud.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.ConfigServerApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
